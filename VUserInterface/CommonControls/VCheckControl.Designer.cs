8e3419b35afd5cad497eb12dd1fd5f2dd6a95d47
Ryan <ryanvandenberg10@gmail.com>
2020-08-27 21:23:17 +1000

    Add VCheckControl

---
+++ VUserInterface/CommonControls/VCheckControl.Designer.cs
@@ -0,0 +1,64 @@
+﻿using System.Windows.Forms;
+
+namespace VUserInterface.CommonControls
+{
+	partial class VCheckControl
+	{
+		/// <summary> 
+		/// Required designer variable.
+		/// </summary>
+		private System.ComponentModel.IContainer components = null;
+
+		/// <summary> 
+		/// Clean up any resources being used.
+		/// </summary>
+		/// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+		protected override void Dispose(bool disposing)
+		{
+			if (disposing && (components != null))
+			{
+				components.Dispose();
+			}
+			base.Dispose(disposing);
+		}
+
+		#region Component Designer generated code
+
+		/// <summary> 
+		/// Required method for Designer support - do not modify 
+		/// the contents of this method with the code editor.
+		/// </summary>
+		private void InitializeComponent()
+		{
+			components = new System.ComponentModel.Container();
+			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+			this.CaptionLabel = new VLabel();
+			this.CheckBox = new VCheckBox();
+			//
+			// this.CaptionLabel
+			//
+			this.CaptionLabel.AutoSize = true;
+			this.CaptionLabel.Location = new System.Drawing.Point(0, 7);
+			this.CaptionLabel.Name = "CaptionLabel";
+			this.CaptionLabel.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
+			this.CaptionLabel.Visible = false;
+			//
+			// CheckBox
+			//
+			this.CheckBox.AutoSize = false;
+			this.CheckBox.DataBindings.Add("CheckState", this, "Checked", true, DataSourceUpdateMode.OnPropertyChanged, CheckState.Indeterminate);
+			this.CheckBox.Location = new System.Drawing.Point(1, 3);
+			this.CheckBox.Size = new System.Drawing.Size(20, 20);
+			//
+			// VCheckBox
+			//
+			this.Controls.Add(CheckBox);
+			this.Controls.Add(CaptionLabel);
+		}
+
+		#endregion
+
+		private Label CaptionLabel;
+		private CheckBox CheckBox;
+	}
+}
---
+++ VUserInterface/CommonControls/VCheckControl.cs
@@ -0,0 +1,118 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Drawing;
+using System.Data;
+using System.Text;
+using System.Windows.Forms;
+
+namespace VUserInterface.CommonControls
+{
+	public partial class VCheckControl : UserControl
+	{
+		public VCheckControl()
+		{
+			InitializeComponent();
+		}
+
+		#region Checked
+
+		public bool Checked
+		{
+			get => fChecked;
+			set
+			{
+				if (value != fChecked)
+				{
+					fChecked = value;
+					CheckBox.Checked = value;
+					OnCheckedChanged();
+				}
+			}
+		}
+
+		bool fChecked;
+
+		#endregion
+
+		#region Caption
+
+		public string Caption
+		{
+			get => fCaption;
+			set
+			{
+				fCaption = value;
+				CaptionLabel.Text = fCaption;
+				CaptionLabel.Visible = !string.IsNullOrEmpty(fCaption);
+				AdjustLabel();
+			}
+		}
+
+		private void AdjustLabel()
+		{
+			var width = CaptionLabel.Width + 5;
+			this.Width += width;
+			this.Left -= width;
+			this.CheckBox.Left += width;
+		}
+
+		string fCaption;
+
+		#endregion
+
+		#region Events
+
+		#region CheckedChanged
+
+		public event EventHandler CheckedChanged;
+
+		void OnCheckedChanged()
+		{
+			CheckedChanged?.Invoke(this, new EventArgs());
+		}
+
+		#endregion
+
+		#endregion
+	}
+
+	class VCheckBox : CheckBox
+	{
+		public VCheckBox()
+		{
+			this.FlatStyle = FlatStyle.Flat;
+		}
+
+		public override bool AutoSize
+		{
+			set { base.AutoSize = false; }
+			get { return base.AutoSize; }
+		}
+
+		protected override void OnPaint(PaintEventArgs e)
+		{
+			e.Graphics.Clear(this.BackColor);
+
+			// create the background
+			var rect = new Rectangle(new Point(0, 0), new Size(this.Width - 1, this.Height - 1));
+			e.Graphics.FillRectangle(Brushes.White, rect);
+			e.Graphics.DrawRectangle(Pens.Black, rect);
+
+			// create the tick
+			if (Checked)
+			{
+				var x1 = 2f * this.Width / 13;
+				var y1 = 5.5f * this.Width / 13;
+				var x2 = 5f * this.Width / 13;
+				var y2 = 8.5f * this.Width / 13;
+				var x3 = 10.5f * this.Width / 13;
+				var y3 = 3f * this.Width / 13;
+
+				var pen = new Pen(Color.Black, 2);
+				e.Graphics.DrawLine(pen, x1, y1, x2, y2);
+				e.Graphics.DrawLine(pen, x3, y3, x2, y2);
+			}
+		}
+	}
+}
