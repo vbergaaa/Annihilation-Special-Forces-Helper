dce5394ce14b79a035f3077b186795ce5fde80e3
Ryan <ryanvandenberg10@gmail.com>
2020-09-01 14:21:38 +1000

    Remove caption code duplication by creating a VUserControl to hold common elements

---
+++ VUserInterface/CommonControls/VUserControl.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Drawing;
+using System.Data;
+using System.Text;
+using System.Windows.Forms;
+
+namespace VUserInterface.CommonControls
+{
+	public partial class VUserControl : UserControl
+	{
+		public VUserControl()
+		{
+			InitializeComponent();
+		}
+
+		#region Caption
+
+		public string Caption
+		{
+			get => fCaption;
+			set
+			{
+				fCaption = value;
+				CaptionLabel.Text = fCaption;
+				CaptionLabel.Visible = !string.IsNullOrEmpty(fCaption);
+				AdjustLabel();
+			}
+		}
+
+		private void AdjustLabel()
+		{
+			var width = CaptionLabel.Width + 5;
+			this.Width += width;
+			this.Left -= width;
+			this.CoreControl.Left += width;
+		}
+
+		string fCaption;
+
+		#endregion
+	}
+}
