81497e80a5712be95638b9b4eaaa8405afecd4d8
Ryan VANDENBERG <ryanvandenberg10@gmail.com>
2020-11-03 21:29:55 +1100

    Redesign Main form

--- VUserInterface/MainForm.Designer.cs
+++ VUserInterface/MainForm.Designer.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.ComponentModel;
+using System.Drawing;
+using System.Windows.Forms;
 using VBusiness.Loadouts;
 using VBusiness.Souls;
 using VUserInterface.CommonControls;
@@ -34,43 +36,157 @@ namespace VUserInterface
 		/// </summary>
 		private void InitializeComponent()
 		{
-			this.LoadoutsLoadList = new VLoadList();
-			this.SoulsLoadList = new VLoadList();
+			this.LoadoutsLoadList = new VUserInterface.CommonControls.VLoadList();
+			this.SoulsLoadList = new VUserInterface.CommonControls.VLoadList();
+			this.TitleLabel = new VUserInterface.CommonControls.VLabel();
+			this.LeftPanel = new System.Windows.Forms.Panel();
+			this.SoulsButton = new System.Windows.Forms.Button();
+			this.LoadoutsButton = new System.Windows.Forms.Button();
+			this.ProfileButton = new System.Windows.Forms.Button();
+			this.RightPanel = new System.Windows.Forms.Panel();
+			this.ProfilePanel = new System.Windows.Forms.Panel();
+			this.LoadoutsPanel = new System.Windows.Forms.Panel();
+			this.SoulsPanel = new System.Windows.Forms.Panel();
+			this.LeftPanel.SuspendLayout();
+			this.RightPanel.SuspendLayout();
+			this.LoadoutsPanel.SuspendLayout();
+			this.SoulsPanel.SuspendLayout();
 			this.SuspendLayout();
-			//
+			// 
 			// LoadoutsLoadList
-			//
-			this.LoadoutsLoadList.BizoType = typeof(Loadout);
-			this.LoadoutsLoadList.Location = new System.Drawing.Point(83, 32);
-			this.LoadoutsLoadList.Name = "LoadoutsListBox";
+			// 
+			this.LoadoutsLoadList.BizoType = typeof(VBusiness.Loadouts.Loadout);
+			this.LoadoutsLoadList.Location = new System.Drawing.Point(50, 30);
+			this.LoadoutsLoadList.Name = "LoadoutsLoadList";
+			this.LoadoutsLoadList.Size = new System.Drawing.Size(280, 160);
 			this.LoadoutsLoadList.TabIndex = 0;
-			this.LoadoutsLoadList.Text = "Loadouts";
-			//
+			// 
 			// SoulsLoadList
-			//
-			this.SoulsLoadList.BizoType = typeof(Soul);
-			this.SoulsLoadList.Location = new System.Drawing.Point(83, 213);
-			this.SoulsLoadList.Name = "SoulsListBox";
+			// 
+			this.SoulsLoadList.BizoType = typeof(VBusiness.Souls.Soul);
+			this.SoulsLoadList.Location = new System.Drawing.Point(50, 30);
+			this.SoulsLoadList.Name = "SoulsLoadList";
+			this.SoulsLoadList.Size = new System.Drawing.Size(280, 160);
 			this.SoulsLoadList.TabIndex = 0;
-			this.SoulsLoadList.Text = "Souls";
+			// 
+			// TitleLabel
+			// 
+			this.TitleLabel.BackColor = System.Drawing.Color.Navy;
+			this.TitleLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 16F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+			this.TitleLabel.ForeColor = System.Drawing.Color.White;
+			this.TitleLabel.Location = new System.Drawing.Point(1, 5);
+			this.TitleLabel.Name = "TitleLabel";
+			this.TitleLabel.Size = new System.Drawing.Size(598, 30);
+			this.TitleLabel.TabIndex = 0;
+			this.TitleLabel.Text = "Annihilation Special Forces Companion Application";
+			this.TitleLabel.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+			// 
+			// LeftPanel
+			// 
+			this.LeftPanel.Controls.Add(this.SoulsButton);
+			this.LeftPanel.Controls.Add(this.LoadoutsButton);
+			this.LeftPanel.Controls.Add(this.ProfileButton);
+			this.LeftPanel.Location = new System.Drawing.Point(0, 35);
+			this.LeftPanel.Name = "LeftPanel";
+			this.LeftPanel.Size = new System.Drawing.Size(200, 400);
+			this.LeftPanel.TabIndex = 0;
+			// 
+			// SoulsButton
+			// 
+			this.SoulsButton.Location = new System.Drawing.Point(12, 133);
+			this.SoulsButton.Name = "SoulsButton";
+			this.SoulsButton.Size = new System.Drawing.Size(182, 31);
+			this.SoulsButton.TabIndex = 2;
+			this.SoulsButton.Text = "Souls";
+			this.SoulsButton.UseVisualStyleBackColor = true;
+			this.SoulsButton.Click += new System.EventHandler(this.SoulsButton_Click);
+			// 
+			// LoadoutsButton
+			// 
+			this.LoadoutsButton.Location = new System.Drawing.Point(12, 96);
+			this.LoadoutsButton.Name = "LoadoutsButton";
+			this.LoadoutsButton.Size = new System.Drawing.Size(182, 31);
+			this.LoadoutsButton.TabIndex = 1;
+			this.LoadoutsButton.Text = "Loadouts";
+			this.LoadoutsButton.UseVisualStyleBackColor = true;
+			this.LoadoutsButton.Click += new System.EventHandler(this.LoadoutsButton_Click);
+			// 
+			// ProfileButton
+			// 
+			this.ProfileButton.Location = new System.Drawing.Point(12, 59);
+			this.ProfileButton.Name = "ProfileButton";
+			this.ProfileButton.Size = new System.Drawing.Size(182, 31);
+			this.ProfileButton.TabIndex = 0;
+			this.ProfileButton.Text = "Profile";
+			this.ProfileButton.UseVisualStyleBackColor = true;
+			this.ProfileButton.Visible = false;
+			this.ProfileButton.Click += new System.EventHandler(this.ProfileButton_Click);
+			// 
+			// RightPanel
+			// 
+			this.RightPanel.Controls.Add(this.ProfilePanel);
+			this.RightPanel.Controls.Add(this.LoadoutsPanel);
+			this.RightPanel.Controls.Add(this.SoulsPanel);
+			this.RightPanel.Location = new System.Drawing.Point(200, 35);
+			this.RightPanel.Name = "RightPanel";
+			this.RightPanel.Size = new System.Drawing.Size(450, 400);
+			this.RightPanel.TabIndex = 1;
+			// 
+			// ProfilePanel
+			// 
+			this.ProfilePanel.Location = new System.Drawing.Point(0, 0);
+			this.ProfilePanel.Name = "ProfilePanel";
+			this.ProfilePanel.Size = new System.Drawing.Size(450, 400);
+			this.ProfilePanel.TabIndex = 1;
+			this.ProfilePanel.Visible = false;
+			// 
+			// LoadoutsPanel
+			// 
+			this.LoadoutsPanel.Controls.Add(this.LoadoutsLoadList);
+			this.LoadoutsPanel.Location = new System.Drawing.Point(0, 0);
+			this.LoadoutsPanel.Name = "LoadoutsPanel";
+			this.LoadoutsPanel.Size = new System.Drawing.Size(450, 400);
+			this.LoadoutsPanel.TabIndex = 1;
+			// 
+			// SoulsPanel
+			// 
+			this.SoulsPanel.Controls.Add(this.SoulsLoadList);
+			this.SoulsPanel.Location = new System.Drawing.Point(0, 0);
+			this.SoulsPanel.Name = "SoulsPanel";
+			this.SoulsPanel.Size = new System.Drawing.Size(450, 400);
+			this.SoulsPanel.TabIndex = 1;
+			this.SoulsPanel.Visible = false;
 			// 
 			// MainForm
 			// 
 			this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
 			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-			this.ClientSize = new System.Drawing.Size(441, 380);
-			this.Controls.Add(this.LoadoutsLoadList);
-			this.Controls.Add(this.SoulsLoadList);
+			this.ClientSize = new System.Drawing.Size(600, 300);
+			this.Controls.Add(this.TitleLabel);
+			this.Controls.Add(this.LeftPanel);
+			this.Controls.Add(this.RightPanel);
 			this.Name = "MainForm";
 			this.Text = "Annihilation Special Forces Companion App";
+			this.LeftPanel.ResumeLayout(false);
+			this.RightPanel.ResumeLayout(false);
+			this.LoadoutsPanel.ResumeLayout(false);
+			this.SoulsPanel.ResumeLayout(false);
 			this.ResumeLayout(false);
-			this.PerformLayout();
 
 		}
 
 		#endregion
 
+		private Panel LeftPanel;
+		private Panel RightPanel;
+		private Panel ProfilePanel;
+		private Panel LoadoutsPanel;
+		private Panel SoulsPanel;
 		private VLoadList LoadoutsLoadList;
 		private VLoadList SoulsLoadList;
+		private VLabel TitleLabel;
+		private Button SoulsButton;
+		private Button LoadoutsButton;
+		private Button ProfileButton;
 	}
 }
--- VUserInterface/MainForm.cs
+++ VUserInterface/MainForm.cs
@@ -17,5 +17,30 @@ namespace VUserInterface
 		{
 			InitializeComponent();
 		}
+
+		void ChangeMainPanel(object sender, EventArgs e)
+		{
+			if (sender is Button button)
+			{
+				ProfilePanel.Visible = button.Name == ProfileButton.Name;
+				LoadoutsPanel.Visible = button.Name == LoadoutsButton.Name;
+				SoulsPanel.Visible = button.Name == SoulsButton.Name;
+			}
+		}
+
+		void LoadoutsButton_Click(object sender, EventArgs e)
+		{
+			ChangeMainPanel(sender, e);
+		}
+
+		void SoulsButton_Click(object sender, EventArgs e)
+		{
+			ChangeMainPanel(sender, e);
+		}
+
+		void ProfileButton_Click(object sender, EventArgs e)
+		{
+			ChangeMainPanel(sender, e);
+		}
 	}
 }
