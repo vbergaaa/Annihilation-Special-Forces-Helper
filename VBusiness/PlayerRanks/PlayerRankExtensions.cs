95de7ec9f7af74fedadb75e6576772e2fc00377f
Ryan VANDENBERG <ryanvandenberg10@gmail.com>
2020-11-09 22:57:42 +1100

    Add Crusader Ranks and Perk Page Limits

---
+++ VBusiness/PlayerRanks/PlayerRankExtensions.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using VEntityFramework.Model;
+
+namespace VBusiness.PlayerRanks
+{
+	public static class PlayerRankExtensions
+	{
+		public static int GetMaxPerkPage(this PlayerRank rank)
+		{
+			return rank switch
+			{
+				PlayerRank.Rookie => 1,
+				PlayerRank.SuperRookie => 2,
+				PlayerRank.PrestigeRookie => 3,
+				PlayerRank.Professional => 4,
+				PlayerRank.SuperProfessional => 5,
+				PlayerRank.PrestigeProfessional => 6,
+				PlayerRank.Crusader => 7,
+				PlayerRank.SuperCrusader => 7,
+				PlayerRank.PrestigeCrusader => 7,
+				PlayerRank.Expert => 8,
+				PlayerRank.SuperExpert => 9,
+				PlayerRank.PrestigeExpert => 10,
+				PlayerRank.Master => 11,
+				PlayerRank.SuperMaster => 12,
+				PlayerRank.PrestigeMaster => 13,
+				_ => 1
+			};
+		}
+	}
+}
