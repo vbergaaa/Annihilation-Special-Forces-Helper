93fae24da4911945790a7da0ec7188e03bfbea28
Ryan VANDENBERG <ryanvandenberg10@gmail.com>
2020-11-29 20:24:30 +1100

    Add a static unt creation method

--- VBusiness/Units/Unit.cs
+++ VBusiness/Units/Unit.cs
@@ -5,6 +5,19 @@ namespace VBusiness.Units
 {
 	public abstract class Unit : VUnit
 	{
+		#region New
+
+		public static Unit New(VEntityFramework.Model.Unit unitType)
+		{
+			return unitType switch
+			{
+				VEntityFramework.Model.Unit.WarpLord => new WarpLord()
+				_ => throw new Exception()
+			};
+		}
+
+		#endregion
+
 		public override double AttackSpeedUpgradeIncrement => 0.96;
 		public override double MaxedAttack => Attack + 100 * AttackUpgradeIncrement;
 		public override double MaxedAttackSpeed => AttackSpeed + Math.Pow(AttackSpeedUpgradeIncrement, 15);
