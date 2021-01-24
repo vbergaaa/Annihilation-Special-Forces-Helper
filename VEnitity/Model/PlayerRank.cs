a491b12783598dd288005de8e7e0526066567cbf
Ryan VANDENBERG <ryanvandenberg10@gmail.com>
2021-01-25 08:28:07 +1100

    Add PlayerRank enum descriptions

--- VEnitity/Model/PlayerRank.cs
+++ VEnitity/Model/PlayerRank.cs
@@ -1,22 +1,34 @@
-﻿namespace VEntityFramework.Model
+﻿using System.ComponentModel;
+
+namespace VEntityFramework.Model
 {
 	public enum PlayerRank
 	{
 		None,
 		Rookie,
+		[Description("Super Rookie")]
 		SuperRookie,
+		[Description("Prestige Rookie")]
 		PrestigeRookie,
 		Professional,
+		[Description("Super Professional")]
 		SuperProfessional,
+		[Description("Prestige Professional")]
 		PrestigeProfessional,
 		Crusader,
+		[Description("Super Crusader")]
 		SuperCrusader,
+		[Description("Prestige Crusader")]
 		PrestigeCrusader,
 		Expert,
+		[Description("Super Expert")]
 		SuperExpert,
+		[Description("Prestige Expert")]
 		PrestigeExpert,
 		Master,
+		[Description("Super Master")]
 		SuperMaster,
+		[Description("Prestige Master")]
 		PrestigeMaster
 	}
 }
