1325047ab922ae5130c0dc86a22399be5a667b57
Ryan VANDENBERG <ryanvandenberg10@gmail.com>
2021-02-06 18:50:49 +1100

    Add Additional Tests for perks

--- Tests/PerksTest.cs
+++ Tests/PerksTest.cs
@@ -1,8 +1,10 @@
-﻿using NUnit.Framework;
+﻿using EnumsNET;
+using NUnit.Framework;
 using System.Collections.Generic;
 using System.Linq;
 using VBusiness.Loadouts;
 using VBusiness.Perks;
+using VBusiness.PlayerRanks;
 using VEntityFramework.Model;
 
 namespace Tests
@@ -20,6 +22,34 @@ namespace Tests
 			{
 				var actualPerk = property.GetValue(perkCollection);
 				Assert.That(perkCollection.AllPerks.Contains(actualPerk), $"{actualPerk.GetType().Name} wasn't found in AllPerks");
+			}
+		}
+
+		[Test]
+		public void TestAllPerkPagesAreImplementedForRank()
+		{
+			var maxPlayerRank = Enums.GetValues<PlayerRank>().Last();
+			var lastPage = maxPlayerRank.GetMaxPerkPage();
+
+			var allPerkPropertyInfos = typeof(PerkCollection).GetProperties().Where(p => p.PropertyType == typeof(VPerk) && !p.Name.StartsWith("Perk")).ToList();
+
+			Assert.That(allPerkPropertyInfos, Has.Count.EqualTo(lastPage * 6),
+				$"Page {lastPage} has been unlocked based on the maximum rank, but you haven't loaded all the perks for that rank. \r\nPlease add the required ranks, fix the max page finding logic, or remove the additional ranks");
+		}
+
+		[Test]
+		public void TestPerksPageAndPostions()
+		{
+			var lastPage = Enums.GetValues<PlayerRank>().Last().GetMaxPerkPage();
+
+			var perkCollection = new PerkCollectionForTest();
+			for (var i = 0; i < lastPage; i++)
+			{
+				for (var j = 0; j < 6; j++)
+				{
+					var matchingPerks = perkCollection.AllPerks.Where(p => p.Position == j + 1 && p.Page == i + 1).ToList();
+					Assert.That(matchingPerks, Has.Count.EqualTo(1), $"Fail at Page {i+1} and Position {j+1}");
+				}
 			}
 		}
 	}
