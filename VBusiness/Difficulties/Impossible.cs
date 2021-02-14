5add71b2a717e9001b980b5b606789f3d23a50e5
Ryan VANDENBERG <ryanvandenberg10@gmail.com>
2021-02-14 11:42:07 +1100

    Add Impossible Difficulty

---
+++ VBusiness/Difficulties/Impossible.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using VEntityFramework.Model;
+
+namespace VBusiness.Difficulties
+{
+	public class Impossible : Difficulty
+	{
+		public override DifficultyLevel Difficulty => DifficultyLevel.Impossible;
+
+		public override int TormentReduction => 17;
+	}
+}
--- VEnitity/Model/VDifficulty.cs
+++ VEnitity/Model/VDifficulty.cs
@@ -40,5 +40,6 @@ namespace VEntityFramework.Model
 		Titanic,
 		Mythic,
 		Divine,
+		Impossible,
 	}
 }
