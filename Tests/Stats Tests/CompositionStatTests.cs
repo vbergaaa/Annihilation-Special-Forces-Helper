68a7109b23ec3636aaa44672d42b0959d8b1d3df
Ryan VANDENBERG <ryanvandenberg10@gmail.com>
2021-07-28 22:04:03 +1000

    Fix failing unit tests

--- Tests/Stats Tests/CompositionStatTests.cs
+++ Tests/Stats Tests/CompositionStatTests.cs
@@ -17,17 +17,17 @@ namespace Tests.Stats_Tests
 		// They are just simply what they were when I made this test,
 		// so I don't accidently change something
 		[TestCase(DifficultyLevel.Brutal,		16126369, 199373199)]
-		[TestCase(DifficultyLevel.Divine,       4343171, 2656003)]
+		[TestCase(DifficultyLevel.Divine,       4078702, 2601555)]
 		[TestCase(DifficultyLevel.Easy,         22886012, 199373199)]
 		[TestCase(DifficultyLevel.Hard,         19251537, 199373199)]
-		[TestCase(DifficultyLevel.Hell,         9150738, 188366064)]
-		[TestCase(DifficultyLevel.Impossible,   2059856, 580488)]
+		[TestCase(DifficultyLevel.Hell,         8960955, 188366064)]
+		[TestCase(DifficultyLevel.Impossible,   1964511, 580488)]
 		[TestCase(DifficultyLevel.Insane,       16973106, 199373199)]
-		[TestCase(DifficultyLevel.Mythic,       6406599, 10327791)]
+		[TestCase(DifficultyLevel.Mythic,       6165723, 10327791)]
 		[TestCase(DifficultyLevel.Nightmare,    13340921, 199373199)]
 		[TestCase(DifficultyLevel.Normal,       22989313, 199373199)]
-		[TestCase(DifficultyLevel.Titanic,      7907784, 39874158)]
-		[TestCase(DifficultyLevel.Torment,      10444208, 191484105)]
+		[TestCase(DifficultyLevel.Titanic,      7676389, 39874158)]
+		[TestCase(DifficultyLevel.Torment,      10287806, 191484105)]
 		[TestCase(DifficultyLevel.VeryEasy,		22889736, 199373199)]
 		[TestCase(DifficultyLevel.VeryHard,		18180084, 199373199)]
 		public void TestCalculatedStats(DifficultyLevel difficulty, double expectedDamage, double expectedToughness)
