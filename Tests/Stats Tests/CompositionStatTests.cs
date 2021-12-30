2d9d80b3d4ac1648933a23dd617d5a1ce8bf6ca0
Ryan VANDENBERG <ryanvandenberg10@gmail.com>
2021-12-30 23:39:02 +1100

    Change Maximum Toughness logic

--- Tests/Stats Tests/CompositionStatTests.cs
+++ Tests/Stats Tests/CompositionStatTests.cs
@@ -10,20 +10,20 @@ namespace Tests.Stats_Tests
 		// These tests are for santity, to make sure damage and toughness calculations
 		// don't unexpectedly change. These values can be changed as more features/bugs
 		// are added or squashed
-		[TestCase(DifficultyLevel.Brutal, 479233813, 199373199)]
+		[TestCase(DifficultyLevel.Brutal, 479233813, 24708953)]
 		[TestCase(DifficultyLevel.Divine, 109388839, 2246008)]
-		[TestCase(DifficultyLevel.Easy, 672830304, 199373199)]
-		[TestCase(DifficultyLevel.Hard, 569359387, 199373199)]
-		[TestCase(DifficultyLevel.Hell, 268748434, 188366064)]
+		[TestCase(DifficultyLevel.Easy, 672830304, 4030798)]
+		[TestCase(DifficultyLevel.Hard, 569359387, 12211993)]
+		[TestCase(DifficultyLevel.Hell, 268748434, 60410747)]
 		[TestCase(DifficultyLevel.Impossible, 44760877, 580488)]
-		[TestCase(DifficultyLevel.Insane, 503662439, 199373199)]
+		[TestCase(DifficultyLevel.Insane, 503662439, 21660446)]
 		[TestCase(DifficultyLevel.Mythic, 175130901, 8173615)]
-		[TestCase(DifficultyLevel.Nightmare, 398052940, 199373199)]
-		[TestCase(DifficultyLevel.Normal, 677217210, 199373199)]
+		[TestCase(DifficultyLevel.Nightmare, 398052940, 37469233)]
+		[TestCase(DifficultyLevel.Normal, 677217210, 6809713)]
 		[TestCase(DifficultyLevel.Titanic, 224390095, 36985862)]
-		[TestCase(DifficultyLevel.Torment, 308575257, 191484105)]
-		[TestCase(DifficultyLevel.VeryEasy, 671014396, 199373199)]
-		[TestCase(DifficultyLevel.VeryHard, 539090062, 199373199)]
+		[TestCase(DifficultyLevel.Torment, 308575257, 46763441)]
+		[TestCase(DifficultyLevel.VeryEasy, 671014396, 2123574)]
+		[TestCase(DifficultyLevel.VeryHard, 539090062, 14410879)]
 		public void TestCalculatedStats(DifficultyLevel difficulty, double expectedDamage, double expectedToughness)
 		{
 			var loadout = LoadMaxPage14();
@@ -40,7 +40,7 @@ namespace Tests.Stats_Tests
 			{
 				perk.DesiredLevel = perk.MaxLevel;
 			}
-			perks.DominatorDamage.DesiredLevel = 50; //lets be realisic
+			perks.DominatorDamage.DesiredLevel = 50;
 			perks.DominatorSpeed.DesiredLevel = 50;
 			loadout.CurrentUnit = VUnit.New(UnitType.BladeMaster, loadout);
 			loadout.CurrentUnit.CurrentInfusion = 10;
